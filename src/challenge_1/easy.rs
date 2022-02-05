4f154db55f8acd14682bb44b86adb7ee74375e8c
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-02-05 12:41:50 -0500

    fixed incomplete failing test for challenge 1 easy

--- src/challenge_1/easy.rs
+++ src/challenge_1/easy.rs
@@ -4,12 +4,14 @@ pub fn solve(name: String, age: String, username: String) -> &'static str {
 
 #[cfg(test)]
 mod tests {
-
+    use super::*;
+    
     #[test]
     fn final_output() {
         let name = String::from("Matthew");
         let age = String::from("17");
         let username = String::from("test-username");
-        assert_eq!(solve(name, age, username), )
+        assert_eq!(solve(name, age, username),
+                   "your name is Matthew, you are 17 years old, and your username is test-username");
     }
 }
